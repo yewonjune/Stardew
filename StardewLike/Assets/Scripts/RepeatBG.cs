dfc63fc6ad9d6a91012ac7960d4c35f62f57afc4
yewonjune <101396034+yewonjune@users.noreply.github.com>
2025-05-27 19:16:09 +0900

    무한 BackGround
    BG 추가 & 무한 BG 코드 작성

---
+++ StardewLike/Assets/Scripts/RepeatBG.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBG : MonoBehaviour
+{
+    [SerializeField][Range(0.1f, 200f)] float speed = 3f;
+
+    float repeatDistance;
+
+    Vector2 startPos;
+
+    float newPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+
+        repeatDistance = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if(transform.position.x <= -repeatDistance)
+        {
+            transform.position += Vector3.right * repeatDistance * 2;
+        }
+    }
+}
