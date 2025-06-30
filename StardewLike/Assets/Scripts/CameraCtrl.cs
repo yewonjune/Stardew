df2ef575c5725db2992e0cd758fc98edb062f3cf
yewonjune <0613jyw@naver.com>
2025-06-30 21:23:28 +0900

    카메라 스크립트 추가
    player 추적 카메라 추가

--- StardewLike/Assets/Scripts/CameraCtrl.cs
+++ StardewLike/Assets/Scripts/CameraCtrl.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraCtrl : MonoBehaviour
 {
+    public Transform player;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,12 @@ public class CameraCtrl : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
     }
 }
